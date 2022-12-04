153c702923cc480ebabeb91e4f3bb646e07add42
AlessioCherici <alessio.cherici@hotmail.it>
2022-12-04 23:14:24 +0100

    Create classi

---
+++ Csharp OOP Agenzia Immobiliare/Appartamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_OOP_Agenzia_Immobiliare
+    {
+    public class Appartamento : Immobile
+        {
+        private int stanze;
+        private int bagni;
+
+        public Appartamento(int stanze, int bagni) : base (string codiceImmobile, string indirizzo, string cap, string città, int metriQuadri)
+            {
+            this.stanze = stanze;
+            this.bagni = bagni;
+            }
+        }
+    }
---
+++ Csharp OOP Agenzia Immobiliare/Immobile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_OOP_Agenzia_Immobiliare
+    {
+    public class Immobile
+        {
+        private string codiceImmobile;
+        private string indirizzo;
+        private string cap;
+        private string città;
+        private int metriQuadri;
+        
+        public Immobile(string codiceImmobile, string indirizzo, string cap, string città, int metriQuadri) 
+            {
+            this.codiceImmobile = codiceImmobile;
+            this.indirizzo = indirizzo;
+            this.cap = cap;
+            this.città = città;
+            this.metriQuadri = metriQuadri;
+            }
+        }
+    }
